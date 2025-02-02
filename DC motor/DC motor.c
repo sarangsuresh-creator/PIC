b4bf9159765ea1717133314be77de1ed26a770b2
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-02 20:09:44 +0530

    Update DC motor.c

--- DC motor/DC motor.c
+++ DC motor/DC motor.c
@@ -11,15 +11,24 @@
 void main(void) {
     TRISB = 0X00;
     PORTB = 0X00;
+    TRISC = 0x01;
+    PORTC = 0X00;
+    int i = 0;
    
     while(1){
+        if(RC0 == 1){
+            i++;
+            while(RC0 == 1);
+        }
+        if(i == 1){
         RB0 = 1;
         RB1 = 0;
-        __delay_ms(3000);
+        }
+        if(i == 2){
         RB0 = 0;
         RB1 = 1;
-        __delay_ms(3000);
+        i = 0;
+        }
     }
     return;
 }
-
