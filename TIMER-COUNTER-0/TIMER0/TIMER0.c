9ac962471c1d815b44fdac8614e1f7225171458a
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-02 20:45:00 +0530

    Update TIMER0.c

--- TIMER-COUNTER-0/TIMER0/TIMER0.c
+++ TIMER-COUNTER-0/TIMER0/TIMER0.c
@@ -1,1 +1,33 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+void timer();
+int i = 0;
+void main(void) {
+    TRISD = 0x00;
+    PORTD = 0X00;
+    T0CON = 0XC7;
+    TMR0L = 60;
+    while(1);
+    timer();
+}
+void timer(){
+    if(TMR0IF == 1){
+        i++;
+        if(i == 100){
+            PORTD =! PORTD;
+            i = 0;
+        }
+        TMR0IF = 0;
+        TMR0L = 60;
+        }
+    }
+
 
