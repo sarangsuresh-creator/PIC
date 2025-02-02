6cca65f16915d7265e56712cfd07c46f5bf44f46
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-02 20:03:03 +0530

    Update DC motor.c

--- DC motor/DC motor.c
+++ DC motor/DC motor.c
@@ -1,1 +1,25 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+void main(void) {
+    TRISB = 0X00;
+    PORTB = 0X00;
+   
+    while(1){
+        RB0 = 1;
+        RB1 = 0;
+        __delay_ms(3000);
+        RB0 = 0;
+        RB1 = 1;
+        __delay_ms(3000);
+    }
+    return;
+}
 
