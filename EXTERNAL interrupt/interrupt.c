67c2d6fd29f604df8f77fb41ba50f6a285786e21
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-02 20:19:35 +0530

    Update interrupt.c

--- EXTERNAL interrupt/interrupt.c
+++ EXTERNAL interrupt/interrupt.c
@@ -1,1 +1,27 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+void  __interrupt() ISR(){
+    if(INT0IF == 1){
+        RD0 =~ RD0;
+        INT0IF = 0;
+    }
+}
+void main(void) {
+    TRISD = 0x00;
+    ADCON1 = 0x0f;
+    GIE = 1;
+    PEIE = 1;
+    INT0IE = 1;
+ 
+    while(1);
+    
+}
 
