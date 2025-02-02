75ed87c75974f1c6a95d11b9e10612c01e7e0f62
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-02 22:08:53 +0530

    Update TIMER3.c

--- TIMER3/TIMER3.c
+++ TIMER3/TIMER3.c
@@ -1,1 +1,35 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+void timer();
+int i = 0;
+void main(void) {
+    TRISD = 0x00;
+    PORTD = 0X00;
+    T3CON = 0X31;
+    TMR3H = 0x00;
+    TMR3L = 0x00;
+    while(1);
+    timer();
+}
+void timer(){
+        if(TMR3IF == 1){
+            i++;
+            if(i==10){
+                PORTD =! PORTD;
+                i=0;
+            }
+            TMR3IF = 0;
+        }
+    }
+        
+ 
+
 
