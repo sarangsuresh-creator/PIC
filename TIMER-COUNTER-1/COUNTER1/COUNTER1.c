7766171b9806265486c93f48278977064c35f909
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-02 22:05:55 +0530

    Update COUNTER1.c

--- TIMER-COUNTER-1/COUNTER1/COUNTER1.c
+++ TIMER-COUNTER-1/COUNTER1/COUNTER1.c
@@ -1,1 +1,29 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+void counter();
+int i = 0;
+void main(void) {
+    TRISD = 0x00;
+    PORTD = 0X00;
+    T1CON = 0X03;
+    while(1);
+    counter();
+}
+void counter(){
+        if(TMR1L == 1){
+            PORTD =! PORTD;
+            TMR1L = 0;
+        }
+    }
+        
+  
+
 
