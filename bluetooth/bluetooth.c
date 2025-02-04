116d27a0abd15eadfcb8e96c5aaeafac0dd12a74
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-04 20:21:49 +0530

    Update bluetooth.c

--- bluetooth/bluetooth.c
+++ bluetooth/bluetooth.c
@@ -1,1 +1,63 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+#include <stdio.h>
+void uart_init(){
+    TXSTA = 0X20;
+    RCSTA = 0X90;
+    SPBRG = 31;
+    TXIF = RCIF = 0;
+}
+void tx(unsigned char a){
+    TXREG = a;
+    while(!TXIF);
+    TXIF = 0;
+}
+unsigned char rx(){
+    while(!RCIF);
+    RCIF = 0;
+    return RCREG;
+}
+void tx_str(unsigned char *s){
+    while(*s){
+        tx(*s++);
+    }
+}
+void main(){
+    PORTB = 0X00;
+    TRISB = 0X00;
+    RC6 = 0;
+    RC7 = 1;
+    uart_init();
+    tx_str("enter the keyword");
+    while(1){
+        tx(rx());
+        if(rx()=='1'){
+            PORTB = 0X01;
+        }
+        if(rx()=='2'){
+            PORTB = 0X02;
+        }
+        if(rx()=='3'){
+            PORTB = 0X04;
+        }
+        if(rx()=='4'){
+            PORTB = 0X08;
+        }
+        if(rx()=='S'){
+            PORTB = 0X00;
+        }
+    }
+}
+
+        
+ 
+
 
