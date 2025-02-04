59099198efc34d2b40e90f30d81518e37c2193e7
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-04 19:07:13 +0530

    Update PWM.c

--- PWM/PWM.c
+++ PWM/PWM.c
@@ -1,1 +1,34 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#include <stdio.h>
+#define _XTAL_FREQ 20000000
+void main(){
+    TRISC = 0X00;
+    T2CON = 0x7e;
+    CCP1CON = 0x2c;
+    PR2 = 255;
+    while(1){
+        CCPR1L = 0x3e;
+        __delay_ms(1000);
+        CCPR1L = 0Xbb;
+        __delay_ms(1000);
+        
+
+    
+    }
+}
+
+    
+
+
+        
+ 
+
 
