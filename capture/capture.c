a16adc74ca88273a4a33f2c97b552fe12e29de2c
SARANG SURESH <sureshsarang44@gmail.com>
2025-02-04 19:18:43 +0530

    Update capture.c

--- capture/capture.c
+++ capture/capture.c
@@ -1,1 +1,26 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+void main(){
+ TRISC = 0Xff;
+ CCP1CON = 0X04;
+ T1CON = 0X79;
+ while(1);
+ return;
+  
+}
+
+    
+
+
+        
+ 
+
 
