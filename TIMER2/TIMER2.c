c50f39771461c8ef9a76eb450a90bc5781747c3f
SARANG SURESH <sureshsarang44@gmail.com>
2025-03-13 21:54:08 +0530

    Update TIMER2.c

--- TIMER2/TIMER2.c
+++ TIMER2/TIMER2.c
@@ -1,1 +1,33 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+void timer();
+void main(void) {
+    TRISD = 0x00;
+    PORTD = 0X00;
+    T2CON = 0Xff;
+    PR2 = 255;
+    
+    
+    while(1){
+    timer();
+    }
+}
+void timer(){
+        if(TMR2IF == 1){
+           
+            PORTD =! PORTD;
+            
+            TMR2IF = 0;
+        }
+    }
+        
+ 
 
