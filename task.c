4e70decad026fe6c83bd8d7063e15561362fe93f
SARANG SURESH <sureshsarang44@gmail.com>
2025-08-20 09:11:46 +0530

    Update task.c

--- task.c
+++ task.c
@@ -1,42 +1,101 @@
+/*
+ * File:   sample.c
+ * Author: LENOVO
+ *
+ * Created on 2 February, 2025, 5:52 PM
+ */
+
+
 #include <xc.h>
+#include <stdbool.h>
 #include <stdio.h>
 #define _XTAL_FREQ 20000000
-int i;
+bool servo_state=false;
+unsigned long adc_val;
+int i=0;
+float angle;
+char arr[20];
+void string(char *s);
+void  __interrupt() ISR(){
+    if(INTCONbits.INT0IF == 1){
+        servo_state =~ servo_state;
+        RC1=servo_state;
+        INTCONbits.INT0IF = 0;
+    }
+    if(PIR1bits.TMR1IF==1){
+        TMR1L = 0X08;
+        TMR1H = 0XCF;
+        RC0=1;
+        PIR1bits.TMR1IF=0;
+        
+    }
+    if(PIR1bits.CCP1IF==1){
+        RC0=0;
+        PIR1bits.CCP1IF=0;
+        
+    }
+     
+}
 
 void main(){
-    TRISC = 0X01;
-    TRISD = 0X00;
-    PORTD = 0X00;
-    PORTC = 0X00;
+    TRISA = 0X01;
+    TRISC = 0X80;
+    TRISB = 0X01;
+    ADCON1 = 0X0E;
+    ADCON2 = 0xA6;
+    ADCON0 = 0x01;
+    T0CON = 0Xc7;
+    T1CON = 0X31;
+    CCP1CON = 0X0a;
+    TXSTA = 0X24;
+    RCSTA = 0X90;
+    SPBRG =129;
+    INTCONbits.GIE = 1;
+    INTCONbits.PEIE = 1;
+    INTCONbits.INT0IE = 1;
+    PIE1bits.TMR1IE=1;
+    PIE1bits.CCP1IE=1;
     while(1){
-        if(RC0==1){
-            i++;
-           while(RC0==1);
-        }
-        if(i==1){
-            PORTD = 0X01;
-        }
-        else if(i==2){
-            PORTD = 0X02;
-        }
-         else if(i==3){
-            PORTD = 0X04;
-        }
-         else if(i==4){
-            PORTD = 0X08;
-        }
-         else if(i==5){
-            PORTD = 0X10;
-        }
-         else if(i==6){
-             PORTD = 0X20;
-         }
-         else if(i==7){
-             PORTD = 0X40;
-         }
-         else if(i==8){
-            PORTD = 0X80;
-            i=0;
-        }
+      if(servo_state==true){
+          ADCON0bits.GODONE=1;
+          while(ADCON0bits.GODONE);
+          adc_val=ADRESL;
+          adc_val |=(ADRESH<<8);
+         unsigned long S_DV = 53622 + (adc_val * 631) / 1023;
+          CCPR1L = (S_DV & 0xff);
+          CCPR1H = ((S_DV>>8)&0xff);
+          angle = (float)(((unsigned long)S_DV-53622)*180)/631;
+           if(INTCONbits.TMR0IF==1){
+          i++;
+          if(i==30){
+              sprintf(arr,"%0.3f\r",angle);
+          string(arr);
+          i=0;
+          }
+        INTCONbits.TMR0IF=0;
+        
+    }
+      } 
+      else{
+          string("servo off\r");
+       
+             CCPR1L = (53622 & 0xff);
+          CCPR1H = ((53622>>8)&0xff);
+      }
+
+  
 }
 }
+    
+void string(char *s){
+    while(*s){ 
+        while(!TXIF);
+        TXREG = *s++;
+       
+       
+    }
+}
+
+        
+ 
+
